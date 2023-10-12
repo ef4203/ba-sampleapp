062bd02321bc1eacdc3affb19182c27710a8021f
Elias Frank <32647755+ef4203@users.noreply.github.com>
2023-10-12 18:36:48 +0200

    Added unit test projects, dockerfile and linters.

---
+++ test/EFK.SampleApp.TelemetryService.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace EFK.SampleApp.TelemetryService.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ test/EFK.SampleApp.Ui.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace EFK.SampleApp.Ui.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
